7ed845c7692c43c3716594aa192a514086387ca5
NickNevius <nnevius@purdue.edu>
2016-02-25 15:15:56 -0500

    Added files via upload
    Multithreaded

--- src/CentralServer.java
+++ src/CentralServer.java
@@ -5,95 +5,154 @@ import javax.net.ssl.SSLServerSocket;
 import javax.net.ssl.SSLServerSocketFactory;
 import javax.net.ssl.SSLSocket;
 
-public class CentralServer {
-
-	
-	private Message m_message;
-	
-	private ServerSocket m_serverSocket;
-	
-	private Socket m_socket;
-	
-	private ObjectOutputStream m_outputStream;
-	
-	private ObjectInputStream m_inputStream;
-	
+public class CentralServer implements Runnable {
+			
 	//private Vector<BaseConfig> configVector;
 	
 	//private Vector<User> userVector;
-	
-	public CentralServer(ServerSocket ss) {
-		this.m_serverSocket = ss;
-	}
-	
-	public Message getMessage() {
-		return this.m_message;
-	}
-	
-	public void recieveMessage() throws ClassNotFoundException, IOException {
-		
-		m_socket = m_serverSocket.accept();
-		
-		m_inputStream = new ObjectInputStream(m_socket.getInputStream());
-	
-		m_message = (Message)m_inputStream.readObject();
-	}
-	
-	public void sendMessage() throws IOException {
 
-		m_outputStream = new ObjectOutputStream(m_socket.getOutputStream());
-
-		m_outputStream.writeObject(m_message);
-		
-		m_outputStream.flush();
-
-	}
+	protected ServerSocket ss = null;
 	
-	
-	public void setMessage(Message m) {
-		this.m_message = m;
-	}
-	
-	public void close() throws IOException
-	{
-		m_inputStream.close();
-		m_outputStream.close();
-		m_socket.close();
+	public CentralServer() {
+	    System.setProperty("javax.net.ssl.keyStore", "mySrvKeystore");
+	    System.setProperty("javax.net.ssl.keyStorePassword", "sensor");
+	    
+	    
+		SSLServerSocketFactory f = (SSLServerSocketFactory)SSLServerSocketFactory.getDefault();
+		try {
+			ss = f.createServerSocket(9999);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public void notifyUsers(Notification notification) {
-	
+		
 	}	
 	
-	public static void main(String[] args) {
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
 		
-	    System.setProperty("javax.net.ssl.keyStore", "mySrvKeystore");
-	    System.setProperty("javax.net.ssl.keyStorePassword", "sensor");
-
 		try {
-			
 			SSLServerSocketFactory f = (SSLServerSocketFactory)SSLServerSocketFactory.getDefault();
 			ServerSocket ss = f.createServerSocket(9999);
 			
-			CentralServer server = new CentralServer(ss);
+			CentralServer server = new CentralServer();
 			do {
 				
-				server.recieveMessage();
+				Socket sock = ss.accept();
+				
+				//creates a new thread that handles that socket
+				new Thread(new ServerListener(sock)).start();
+				
+				/*
+				server.recieveMessage(sock);
 				
 				Message tmp = server.getMessage();
 				
 				System.out.println("Recieved: " + tmp.getString() + " from client");
 				
-				server.setMessage(new Message("F*#@ED UP THE MESSAGE", null, null));
+				server.setMessage(new Message("F*#@ED UP THE MESSAGE", tmp.type));
 				
 				server.sendMessage();
 				
 				server.close();
+				*/
 				
 			} while (!server.getMessage().getString().equalsIgnoreCase("quit"));
 			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
 	}
+	
+	
+	
+	public class ServerListener implements Runnable {
+
+		protected Socket sock = null;
+		protected ObjectOutputStream out = null;
+		protected ObjectInputStream in = null;
+		
+		public ServerListener(Socket sock) {
+			this.sock = sock;
+			try {
+				out = new ObjectOutputStream(sock.getOutputStream());
+				in = new ObjectInputStream(sock.getInputStream());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		public Message receiveMessage() throws ClassNotFoundException, IOException {
+			return (Message)in.readObject();
+		}
+		
+		public void sendMessage(Message msg, Socket sock) throws IOException {
+			out.writeObject(msg);
+			out.flush();
+		}
+		
+		public void close() throws IOException
+		{
+			in.close();
+			out.close();
+			sock.close();
+		}
+		
+		
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+		
+			try {
+
+				//do shit
+				
+				try {
+					Message msg = receiveMessage();
+					
+					switch(msg.type) {
+					
+					case VIDEO:
+						break;
+					case AUDIO:
+						break;
+					case LIGHT:
+						break;
+					case CONFIG:
+						break;
+					default:
+						break;
+						
+					}
+					
+				} catch (ClassNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				
+					
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+			
+			
+			
+		}
+		
+	}
+	
+	
+	
+	
 }
