ab39dde63cfa3d426e7669d4bf218307768fd59c
Dhairya Doshi <doshid@purdue.edu>
2016-02-28 15:55:40 -0500

    More basic methods added for DB

--- src/DBConnect.java
+++ src/DBConnect.java
@@ -7,7 +7,7 @@ import java.sql.ResultSet;
 public class DBConnect {
 	private static Connection con;
 	
-	public static String returnEntry (String columnName) throws SQLException {
+	public static int getColumnNumber(String columnName) {
 		int columnNumber = 0;
 		if (columnName == "USERNAME") {
 			columnNumber = 1;
@@ -18,12 +18,32 @@ public class DBConnect {
 		else if (columnName == "SENSOR_NAME") {
 			columnNumber = 3;
 		}
+		else if (columnName == "CONFIG") {
+			columnNumber = 4;
+		}
+		else if (columnName == "DETECTION") {
+			columnNumber = 5;
+		}
 		else if (columnName == "S_TYPE") {
 			columnNumber = 6;
 		}
-		else {
-			return null;
+		else if (columnName == "S_LOWER") {
+			columnNumber = 7;
 		}
+		else if (columnName == "S_UPPER") {
+			columnNumber = 8;
+		}
+		else if (columnName == "DETECTION_DATE") {
+			columnNumber = 9;
+		}
+		else if (columnName == "TIME_STAMP") {
+			columnNumber = 10;
+		}
+		return columnNumber;
+	}
+	
+	public static String returnEntry (String columnName) throws SQLException {
+		int columnNumber = getColumnNumber(columnName);
 		ResultSet rs = connect();
 		
 		if (rs == null) {
@@ -35,6 +55,63 @@ public class DBConnect {
 		}
 	}
 	
+	public static float returnLower() throws SQLException {
+		int columnNumber = getColumnNumber("S_LOWER");
+		ResultSet rs = connect();
+		
+		if (rs == null) {
+			return 0;
+		}
+		else {
+			float result = rs.getFloat(columnNumber);
+			return result;
+		}
+	}
+	
+	public static float returnUpper() throws SQLException {
+		int columnNumber = getColumnNumber("S_UPPER");
+		ResultSet rs = connect();
+		
+		if (rs == null) {
+			return 0;
+		}
+		else {
+			float result = rs.getFloat(columnNumber);
+			return result;
+		}
+	}
+	
+	public static boolean isConfig() throws SQLException {
+		int columnNumber = getColumnNumber("CONFIG");
+		ResultSet rs = connect();
+		
+		if (rs == null) {
+			return false;
+		}
+		else {
+			int result = rs.getInt(columnNumber);
+			if (result == 0)
+				return false;
+			else
+				return true;
+		}
+	}
+	
+	public static boolean isDetection() throws SQLException {
+		int columnNumber = getColumnNumber("DETECTION");
+		ResultSet rs = connect();
+		
+		if (rs == null) {
+			return false;
+		}
+		else {
+			int result = rs.getInt(columnNumber);
+			if (result == 0)
+				return false;
+			else
+				return true;
+		}
+	}
 	
 	public static ResultSet connect() {
 
