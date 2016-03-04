b30467772260e43664c75d431df82c1259948654
NickNevius <joe@10.184.222.142>
2016-03-03 19:12:14 -0500

    things and stuff. but mostly stuff.

--- src/RaspberryPi.java
+++ src/RaspberryPi.java
@@ -120,26 +120,32 @@ public class RaspberryPi {
 		}
 	}
 
-	// run the pi?
-	public void run() throws InterruptedException {
 
+	
+	
+	public static void main(String[] args) throws IOException, InterruptedException {
+		
+		RaspberryPi pi = new RaspberryPi();
 		
 		// main loop
 		while (true) {
 			//System.out.println("from sense thread");
-			if (this.sensor == null) { // if pi doesnt have a sensor
+			if (pi.sensor == null) { // if pi doesnt have a sensor
 				Thread.sleep(5000);
 				continue;
 			} else {
-				if (isSensorActive()) {
-					this.sensor.sense(); // tell sensor to sense shit maybe a
+				if (pi.sensor.isSensorActive()) {
+					pi.sensor.sense(); // tell sensor to sense shit maybe a
 					Thread.sleep(1000);		// time interval in between, currently one second
-					if (this.sensor.check_threshold() || streaming) { //if the threshold is above, or if we are supposed to stream constantly
-						send_message(this.sensor.form_message());
+					if (pi.sensor.check_threshold() || pi.streaming) { //if the threshold is above, or if we are supposed to stream constantly
+						pi.send_message(pi.sensor.form_message());
 					}
 				}
 			}
 		}
+		
 	}
+	
+	
 
 }
