0e3f6d9d71bc5ba0caa83c912bd368abcb126c69
Isaac Lepow <isaac.lepow34@gmail.com>
2016-03-09 18:58:43 -0500

    Added Twilio example. Removed example message class due to conflict.

---
+++ src/Example/TwilioTest.java
@@ -0,0 +1,36 @@
+package Example;
+
+//You may want to be more specific in your imports 
+import java.util.*; 
+import org.apache.http.*;
+import org.apache.http.message.*;
+import com.twilio.sdk.*; 
+import com.twilio.sdk.resource.factory.*; 
+import com.twilio.sdk.resource.instance.*; 
+import com.twilio.sdk.resource.list.*; 
+
+public class TwilioTest { 
+// Find your Account Sid and Token at twilio.com/user/account 
+public static final String ACCOUNT_SID = "ACc00d942b6e40dd03ef247140a4638d15"; 
+public static final String AUTH_TOKEN = "1e705b0e12a0e93aa06f692e659feffa"; 
+
+public static void main(String[]args) throws TwilioRestException { 
+	Scanner scanner = new Scanner(System.in);
+	System.out.println("Please enter a phone number");
+	String phoneNumber = scanner.nextLine();
+	System.out.println("Type your message here");
+	String textMessage = scanner.nextLine();
+	TwilioRestClient client = new TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN); 
+
+	 // Build the parameters 
+	 List<NameValuePair> params = new ArrayList<NameValuePair>(); 
+	 params.add(new BasicNameValuePair("To", phoneNumber)); 
+	 params.add(new BasicNameValuePair("From", "+17655889993")); 
+	 params.add(new BasicNameValuePair("Body", textMessage));   
+
+	 MessageFactory messageFactory = client.getAccount().getMessageFactory(); 
+	 Message message = messageFactory.create(params); 
+	 System.out.println(message.getSid()); 
+	 scanner.close();
+	} 
+}
