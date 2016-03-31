d09c803038297cc5701f9f36ed18f300d4dad2a2
Dhairya Doshi <doshid@purdue.edu>
2016-03-31 17:19:26 -0400

    Refactored code with changes to file organization

--- src/AutoAwareApp/app/src/main/java/cs307/purdue/edu/autoawareapp/Sensor.java
+++ src/AutoAwareApp/app/src/main/java/cs307/purdue/edu/autoawareapp/Sensor.java
@@ -5,13 +5,12 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class Sensor extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.sensor);
     }
 
     @Override
