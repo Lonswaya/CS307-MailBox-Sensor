053145c43d6f68e173de84bd69daeb93d59d678b
Zixuan Song <song302@purdue.edu>
2016-04-02 22:08:18 -0400

    threaded

--- src/AutoAwareApp/app/src/main/java/cs307/purdue/edu/autoawareapp/Server.java
+++ src/AutoAwareApp/app/src/main/java/cs307/purdue/edu/autoawareapp/Server.java
@@ -9,45 +9,99 @@ import javax.net.ssl.SSLSocketFactory;
 /**
  * Used to grab sensor infos from central server
  */
-public class Server implements Runnable{
+public class Server implements Runnable {
     public ArrayList<SensorInfo> sensorList;
     public String server_ip;
     public int server_port;
 
-    public Server(){
+    public Server() {
         System.setProperty("javax.net.ssl.trustStore", "mySrvKeystore");  //get ssl working
         System.setProperty("javax.net.ssl.trustStorePassword", "sensor"); //get ssl working
     }
 
-    public void run(){
+    public void run() {
+        int interval = 5000;
 
+        long lastMilli = System.currentTimeMillis();
+        while(true){
+            if( (int) (System.currentTimeMillis() - lastMilli) >= interval) break;
+        }
+
+        this.sensorList = getSensors();
     }
 
-    public ArrayList<SensorInfo> getSensors(){
+    public ArrayList<SensorInfo> getSensors() {
         ArrayList<SensorInfo> list = null;
-        Socket sendSocket;
-        ServerSocket receiveSocket;
-        SSLSocketFactory factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
-        SSLServerSocketFactory serverfactory = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
+        SSLServerSocketFactory factory = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
+        Message msg;
         try {
-
-            ObjectOutputStream outputStream;
-            sendSocket = factory.createSocket(server_ip, server_port);
-            outputStream = new ObjectOutputStream(sendSocket.getOutputStream());
-            SensorsMessage msg = new SensorsMessage("Get sensors", null);
-            outputStream.writeObject(msg);
-            outputStream.flush();
-            outputStream.close();
-            sendSocket.close();
-            receiveSocket = serverfactory.createServerSocket(server_port);
-            sendSocket = receiveSocket.accept();
-            ObjectInputStream inputStream = new ObjectInputStream(sendSocket.getInputStream());
-            msg = (SensorsMessage) inputStream.readObject();
-            list = msg.ar;
+            ServerSocket server = factory.createServerSocket(server_port);
+            msg = new SensorsMessage("Get sensors", null);
+            GetMessage getThread = new GetMessage(msg, server);
+            getThread.start();
+            new SendMessage(msg, server_ip, server_port).start();
+            msg = null;
+            while(msg == null) msg = getThread.getMessage();
+            if(msg.type == MessageType.GET_SENSORS){
+                SensorsMessage sMsg = (SensorsMessage) msg;
+                list = sMsg.ar;
+            }
         }catch(Exception e){
             e.printStackTrace();
         }
-
         return list;
+    }
+
+    class SendMessage extends Thread {
+        public Message msg;
+        public String ip;
+        public int port;
+
+        public SendMessage(Message msg , String ip, int port) {
+            this.msg = msg;
+            this.ip = ip;
+            this.port = port;
+        }
+
+        public void run() {
+            SSLSocketFactory factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+            try {
+                Socket socket = factory.createSocket(ip, port);
+                ObjectOutputStream out;
+                out = new ObjectOutputStream(socket.getOutputStream());
+                out.writeObject(msg);
+                out.flush();
+                out.close();
+                socket.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    class GetMessage extends Thread{
+        public Message msg;
+        public ServerSocket serverSocket;
+
+        public GetMessage(Message msg, ServerSocket serverSocket){
+            this.msg = msg;
+            this.serverSocket = serverSocket;
+        }
+        public Message getMessage(){
+            return this.msg;
+        }
+        public void run(){
+            try{
+                Socket socket = serverSocket.accept();
+                ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+                this.msg = (SensorsMessage) in.readObject();
+                //serverSocket.close();
+                in.close();
+                socket.close();
+                serverSocket.close();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 }
