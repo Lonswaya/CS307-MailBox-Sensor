ce0b1b55821d6c9a4cb764ad046154fc803440fe
Isaac Lepow <isaac.lepow34@gmail.com>
2016-04-17 13:04:21 -0500

    Actual exception handling for sender

--- src/SeparateServer/Sender.java
+++ src/SeparateServer/Sender.java
@@ -3,9 +3,13 @@ import java.io.IOException;
 import cs307.purdue.edu.autoawareapp.*;
 
 public class Sender {
-	public static void send(String email, String message) throws IOException{
+	public static void send(String email, String message) {
 		message = "\"" + message + "\"";
 		ProcessBuilder pb = new ProcessBuilder("sendmail.sh", email, message);
-		Process p = pb.start();	
+		try {
+			Process p = pb.start();	
+		} catch (IOException e) {
+			System.out.println(e.toString());
+		}
 	}
 }
