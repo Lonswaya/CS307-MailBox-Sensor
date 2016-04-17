07fd3addb7190a5aa70e2d3330c3200005d4af4e
Isaac Lepow <isaac.lepow34@gmail.com>
2016-04-17 15:46:09 -0500

    Sender now sends through Gmail

--- src/Example/Sender.java
+++ src/Example/Sender.java
@@ -1,28 +1,43 @@
 package Example;
+import com.sun.mail.smtp.SMTPTransport;
+import java.security.Security;
+import java.util.Date;
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.PasswordAuthentication;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.net.Socket;
-import java.net.UnknownHostException;
 
 public class Sender {
-	
-	final static int DEFAULT_MIDDLEMAN_PORT = 6969;	
-	
-	public void run() {
-		while(true) {
-			Message msg = new Message("Made message");
-			Socket sock = Connections.getSocket("localhost", DEFAULT_MIDDLEMAN_PORT);
-			Connections.send(sock, msg);
-			System.out.println("Sent message");
-			Connections.closeSocket(sock);
+	public static void send(String email, String message) {
+		Properties properties = System.getProperties();
+		properties.setProperty("mail.smtp.auth", "true");
+		properties.setProperty("mail.smtp.starttls.enable", "true");
+		properties.setProperty("mail.smtp.host", "smtp.gmail.com");
+		properties.setProperty("mail.smtp.port", "587");
+		Session session = Session.getDefaultInstance(properties,
+			new javax.mail.Authenticator() {
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication("AutoAwareCS307@gmail.com", "MailboxSensor");
+				}
+			}
+		);
+		try {
+			MimeMessage mm = new MimeMessage(session);
+			mm.setFrom(new InternetAddress("AutoAwareCS307@gmail.com", "AutoAware"));
+			mm.addRecipient(javax.mail.internet.MimeMessage.RecipientType.TO, new InternetAddress(email));
+			mm.setSubject("AutoAware Notification");
+			mm.setText(message);
+			Transport.send(mm);
+			System.out.println("sent email to " + email);
+		} catch (Exception e) { //address or messaging exception
+			e.printStackTrace();
 		}
 	}
 	
-	
-	public static void main(String[] args) {
-		new Sender().run();
-	}
-	
-
 }
