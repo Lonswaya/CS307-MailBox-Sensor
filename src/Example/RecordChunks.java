70f7225309fd2e0a06658f229b72363898d3e3cf
Sai-Thippasani <sthippas@purdue.edu>
2016-04-24 15:37:54 -0400

    Slight changes still doesnt work

--- src/Example/RecordChunks.java
+++ src/Example/RecordChunks.java
@@ -15,6 +15,9 @@ import javax.sound.sampled.DataLine;
 import javax.sound.sampled.TargetDataLine;
 
 import javax.sound.sampled.AudioFileFormat.Type;
+
+import org.apache.commons.lang3.ArrayUtils;
+
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
@@ -25,14 +28,17 @@ import javax.sound.sampled.TargetDataLine;
 
 public class RecordChunks
 {
-	static InputStream in = null;
-	static AudioInputStream stream = null;
+	//static InputStream in = null;
+	//static AudioInputStream stream = null;
+	//static int maxArraySize = 2147483647;
+	//static byte[] massive = new byte[999999999];
 	
-
+	static ByteArrayOutputStream out = new ByteArrayOutputStream();
+	static AudioFormat format = new AudioFormat(44100.0f, 16, 2, true, false);
+	
+	//Method that record chunks of audio and saves it to a wav file
 	public static void recordChunks()
 	{
-		AudioFormat format = new AudioFormat(44100.0f, 16, 2, true, false);
-		
 		TargetDataLine microphone;	//Read sound into this
 		
 		try{
@@ -42,7 +48,7 @@ public class RecordChunks
 			microphone = (TargetDataLine) AudioSystem.getLine(info);
 			microphone.open(format);
 			
-			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			
 			int numBytesRead = 0;
 			int CHUNK_SIZE = 1024;
 			byte[] data = new byte[microphone.getBufferSize()/5];
@@ -58,31 +64,11 @@ public class RecordChunks
 				//write the microphone data to a stream for use later
 				System.out.println("writing outputstream");
 				out.write(data, 0, numBytesRead);
-				
-				byte[] audioBytes = out.toByteArray();
-				 in = new ByteArrayInputStream(audioBytes);
-			     stream = new AudioInputStream(in, format, audioBytes.length);
-				
+				//Add the bytes that were read to the massive byte array
+				//massive = (byte[])ArrayUtils.addAll(massive, data);
 			}
 			microphone.close();
-			
-			//Convert the output stream to a byte array and make audiostream
-			/*byte[] audioBytes = out.toByteArray();
-			InputStream in = new ByteArrayInputStream(audioBytes);
-		    AudioInputStream stream = new AudioInputStream(in, format, audioBytes.length);
-		    */
-		    //Save to a wav file here
-		    String path = System.getProperty("user.home");
-		    System.out.println(path);
-		    try{
-		    	File file = new File (path + File.separator + "ChunkRecording.wav");
-		    	AudioSystem.write(stream,  Type.WAVE, file);
-		    }
-		    catch (Exception e)
-		    {
-		    	e.printStackTrace();
-		    }
-			
+		    
 		}
 		catch(Exception e)
 		{
@@ -92,11 +78,56 @@ public class RecordChunks
 		
 		
 	}
+	//The method that will write the massive byte array to a wav file.
+	public static void writeToWAV()
+	{
+		//Convert the output stream to a byte array and make audiostream
+		byte[] audioBytes = out.toByteArray();
+		InputStream in = new ByteArrayInputStream(audioBytes);
+	    AudioInputStream stream = new AudioInputStream(in, format, audioBytes.length);
+	    
+	    //Save to a wav file here
+	    String path = System.getProperty("user.home");
+	    System.out.println(path);
+	    try{
+	    	File file = new File (path + File.separator + "ChunkRecording.wav");
+	    	AudioSystem.write(stream,  Type.WAVE, file);
+	    }
+	    catch (Exception e)
+	    {
+	    	e.printStackTrace();
+	    }
+		
+	    
+	}
 	
 	public static void main(String [] args)
 	{
-		while(true) 
-			recordChunks();
+		long begin = System.currentTimeMillis();
+		
+		
+		while(true)
+		{
+			long current = System.currentTimeMillis();
+			long difference = (current-begin)/100;
+			
+			double duration = 6;
+			
+			if(difference > duration)
+			{
+				break;
+			}
+			else
+			{	
+				System.out.println("recording a chunk of audio");
+				recordChunks();
+			}
+		}
+		
+		System.out.println("done recording, write to wav");
+		writeToWAV();
+		
+		
 	}
 	
 	
